3041fd3e4054f7266f8438428cd094317e192420
SzymokArch <94990211+SzymokArch@users.noreply.github.com>
2023-03-08 11:23:28 +0100

    Add files via upload
    Now works :)

--- value_noise.c
+++ value_noise.c
@@ -7,48 +7,52 @@ float linear_interpolate(float a, float b, float x){
   return a * (1 - x) + b * x;
 }
 
-void interpolate_2D(float ** map, int start_y, int start_x, int end_y, int end_x, short type){
-  for (int i = 0; i <= end_y; i ++){
-    map[i][0] = linear_interpolate(map[start_y][start_x], map[end_y][start_x], (double)i/(end_y));
-    map[i][end_x] = linear_interpolate(map[start_y][end_x], map[end_y][end_x], (double)i/(end_y));
-    for (int j = 0; j < end_x; j ++){
-      map[i][j] = linear_interpolate(map[i][start_x], map[i][end_x], (double)j/(end_x));
+void interpolate_2D(float ** map, vec2 start, vec2 end){
+  for (int i = start.y; i <= end.y; i ++){
+    map[i][start.x] = linear_interpolate(map[start.y][start.x], map[end.y][start.x], (double)(i - start.y)/(end.y - start.y));
+    map[i][end.x] = linear_interpolate(map[start.y][end.x], map[end.y][end.x], (double)(i - start.y)/(end.y - start.y));
+    for (int j = start.x; j < end.x; j ++){
+      map[i][j] = linear_interpolate(map[i][start.x], map[i][end.x], (double)(j - start.x)/(end.x - start.x));
     }
   }
 
 }
 
+void init_corner_values(float_map noise_map, float_map target_map, int iteration){
+  int pitch = (target_map.height - 1)/pow(2, iteration);
+  if (pitch < 1){
+    return;
+  }
+  for (int i = 0; i < target_map.height; i += pitch){
+    for (int j = 0; j < target_map.width; j += pitch){
+      target_map.map[i][j] = noise_map.map[i][j];
+    }
+  }
+}
+
+void interpolate_between_corners(float_map target_map, int pitch){
+  for (int i = 0; i < target_map.height - pitch; i += pitch){
+    for (int j = 0; j < target_map.width - pitch; j += pitch){
+      interpolate_2D(target_map.map, get_vec2(j, i), get_vec2(j + pitch, i + pitch));
+    }
+  }
+}
+
 // The array dimensions should be equal to 2^n + 1
-float_map value_noise(float_map noise_map, int octaves, double persistence){
+// Generates a single octave of value noise
+float_map value_noise(float_map noise_map, int iteration){
   float_map gen_map;
   gen_map.height = noise_map.height;
   gen_map.width = noise_map.width;
   gen_map.map = (float **)calloc(gen_map.height, sizeof(float *));
   for (int i = 0; i < gen_map.height; i ++){
-      gen_map.map[i] = (float *)calloc(gen_map.height, sizeof(float));
+    gen_map.map[i] = (float *)calloc(gen_map.height, sizeof(float));
   }
 
-  double p = 1;
-  int temp_y;
-  int temp_x;
-  for (int o = 0; o < octaves; o ++){
-    temp_y = (int)(gen_map.height - 1)/pow(2, o);
-    temp_x = (int)(gen_map.width - 1)/pow(2, o);
-    if (temp_y < 1 || temp_x < 1){
-        break;
-    }
-    for (int i = 0; i < gen_map.height; i += temp_y){
-      for (int j = 0; j < gen_map.width; j += temp_x){
-        gen_map.map[i][j] = gen_map.map[i][j] + (noise_map.map[i][j] / p);
-      }
-    }
-    for (int i = 0; i < gen_map.height - temp_y; i += temp_y){
-      for (int j = 0; j < gen_map.width - temp_x; j += temp_x){
-        interpolate_2D(gen_map.map, i, j, i + temp_y, j + temp_x, 1);
-      }
-    }
-      p /= persistence;
-  }
+  init_corner_values(noise_map, gen_map, iteration);
+  int pitch = (gen_map.height - 1)/pow(2, iteration);
+  interpolate_between_corners(gen_map, pitch);
 
   return gen_map;
 }
+
