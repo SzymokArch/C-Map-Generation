9bc6743a5f78b7e9505a04c942c393929c378ec4
SzymokArch <94990211+SzymokArch@users.noreply.github.com>
2023-03-15 16:52:00 +0100

    Add files via upload

--- voronoi.c
+++ voronoi.c
@@ -1,6 +1,49 @@
 #include <stdlib.h>
 #include "distance.c"
 
+vec2 get_center(vec2 start, vec2 end){
+  int x = abs(end.x - start.x)/2;
+  int y = abs(end.y - start.y)/2;
+  return get_vec2(x, y);
+}
+
+vec2 * get_center_array(short_map map, short iterations){
+  int center_quantity = pow(4, iterations);
+  vec2 * center_array = (vec2 *)calloc(center_quantity, sizeof(vec2));
+  int pitch = (map.height - 1)/pow(2, iterations);
+  short index = 0;
+  for (int i = 0; i < map.height - pitch; i += pitch){
+    for (int j = 0; j < map.width - pitch; j += pitch){
+      center_array[index] = get_center(get_vec2(j, i), get_vec2(j + pitch, i + pitch));
+      index ++;
+    }
+  }
+  return center_array;
+}
+
+void move_centers(int seed, vec2 * center_array, int center_quantity){
+  srand(seed);
+  bool movement_down;
+  bool movement_right;
+  int max_movement = center_array[0].x;
+  for (int i = 0; i < center_quantity; i ++){
+    movement_down = rand()%101 > 50;
+    movement_right = rand()%101 > 50;
+    if (movement_down){
+      center_array[i].y += rand()%max_movement;
+    }
+    else{
+      center_array[i].y -= rand()%max_movement;
+    }
+    if (movement_right){
+      center_array[i].x += rand()%max_movement;
+    }
+    else{
+      center_array[i].x -= rand()%max_movement;
+    }
+  }
+}
+
 // gives the index of the closest generator to the given point
 int get_closest_gen(vec2 * generators, int generator_quantity, vec2 point, int p){
   float * distances = (float *)calloc(generator_quantity, sizeof(float));
@@ -42,16 +85,27 @@ int get_closest_gen(vec2 * generators, int generator_quantity, vec2 point, int p
 // The algorithm uses an extremely slow aproach to calculating distance, (checking every generator with every point in the array)
 // I don't really understand fortune's algorithm, and it is only used to generate a diagram with euclidean distance anyway
 // TODO Implement fortune's algorithm to generate the diagram when p = 2 (euclidean distance)
-short_map generate_voronoi_diagram(vec2 * generators, int generator_quantity, vec2 dimensions, int p){
-  short_map diagram;
-  diagram.height = dimensions.y;
-  diagram.width = dimensions.x;
-  diagram.map = (short **)calloc(diagram.height, sizeof(short *));
-  for (int i = 0; i < diagram.height; i ++){
-    diagram.map[i] = (short *)calloc(diagram.width, sizeof(short));
-    for (int j = 0; j < diagram.width; j ++){
-      diagram.map[i][j] = get_closest_gen(generators, generator_quantity, get_vec2(j, i), p);
+void voronoi_diagram(short_map map, vec2 * generators, int generator_quantity, int p){
+  for (int i = 0; i < map.height; i ++){
+    for (int j = 0; j < map.width; j ++){
+      map.map[i][j] = get_closest_gen(generators, generator_quantity, get_vec2(j, i), p);
     }
   }
-  return diagram;
+}
+
+short_map generate_voronoi_diagram(int seed, int size, int divisions, int p){
+  short_map map;
+  map.height = size;
+  map.width = size;
+  map.map = (short **)calloc(size, sizeof(short *));
+  for (int i = 0; i < size; i ++){
+    map.map[i] = (short *)calloc(size, sizeof(short));
+  }
+  vec2 * center_array = get_center_array(map, divisions);
+  int center_quantity = pow(4, divisions);
+  move_centers(seed, center_array, center_quantity);
+  voronoi_diagram(map, center_array, center_quantity, p);
+  free(center_array);
+
+  return map;
 }
